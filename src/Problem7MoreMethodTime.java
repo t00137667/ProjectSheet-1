9f9d7b6e6f7208b063729fbfce4544ede061154a
t00137667 <t00137667@gmail.com>
2018-09-20 09:33:59 +0100

    Problem7 Complete

--- src/Problem7MoreMethodTime.java
+++ src/Problem7MoreMethodTime.java
@@ -1,2 +1,27 @@
+import javax.swing.*;
+
 public class Problem7MoreMethodTime {
+    public static void main(String[] args) {
+        int input;
+
+        input = Integer.parseInt(JOptionPane.showInputDialog("Please enter an integer"));
+
+        while (input !=-1) {
+            if (isEven(input)) {
+                JOptionPane.showMessageDialog(null, input + " is even");
+            } else {
+                JOptionPane.showMessageDialog(null, input + " is odd");
+            }
+            input = Integer.parseInt(JOptionPane.showInputDialog("Please enter an integer"));
+        }
+    }
+    private static boolean isEven(int i)
+    {
+        if(i%2==0)
+        {
+            return true;
+        }
+        else
+            return false;
+    }
 }
