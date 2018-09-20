33a05309d346214a6ff33b4d90ead09947d20f91
t00137667 <t00137667@gmail.com>
2018-09-20 09:54:10 +0100

    Problem8 Complete

--- src/Problem8Arrays.java
+++ src/Problem8Arrays.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
+
 public class Problem8Arrays {
+    static int[] array = new int[10];
+
+    public static void main(String[] args) {
+        arrayInput(array);
+        System.out.print("The first value in the array is "+array[0]);
+        System.out.print("\n\nThe fifth value in the array is "+array[4]);
+    }
+    private static void arrayInput(int[] array)
+    {
+        Scanner input = new Scanner(System.in);
+        for(int i=0;i<10;i++)
+        {
+            System.out.println("Please enter an integer: ");
+            array[i] = input.nextInt();
+        }
+    }
 }
